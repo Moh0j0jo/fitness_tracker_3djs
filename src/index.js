73bc5981b7477c56a09be3e09c11e4429178ea10
Peter Pinnel <peter.pinnel@gmail.com>
2023-08-22 14:46:58 +0200

    fb snapshot code & update

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import { legendColor } from 'd3-svg-legend'
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBd3qu-NGBxA0qnQhVC2bs0rjmdP7sYi1w",
-  authDomain: "udemy-practice-e0102.firebaseapp.com",
-  projectId: "udemy-practice-e0102",
-  storageBucket: "udemy-practice-e0102.appspot.com",
-  messagingSenderId: "145692816151",
-  appId: "1:145692816151:web:e75328324d02dd9a21fd52",
-  measurementId: "G-7M6DXMV50F"
+  apiKey: "AIzaSyDMw--Z3Zl-e-y1TUnXMmghE5Dd7Gl5ChE",
+  authDomain: "udemy-d3-firebase-23286.firebaseapp.com",
+  projectId: "udemy-d3-firebase-23286",
+  storageBucket: "udemy-d3-firebase-23286.appspot.com",
+  messagingSenderId: "124615150939",
+  appId: "1:124615150939:web:2408f5cbfdc3b3b9a1de7e",
+  measurementId: "G-F3K0M89R9G"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -53,20 +53,84 @@ form.addEventListener('submit', e => {
   e.preventDefault()
 
   const distance = parseInt(input.value);
+
   if(distance){
-    db.collection('activities').add({
+
+    const item = {
       distance, 
       activity,
       date: new Date().toString()
-    }).then(() => {
-      error.textContent = '';
-      input.value = '';
-    }).catch(err => console.log(err));
+    }
+
+    addDoc(colRef, item)
+      .then(() => {
+        error.textContent = '';
+        input.value = '';
+      })
+      .catch(err => console.log(err));
   } else {
     error.textContent = 'Please enter a valid distance'
   }
 
 });
+
+const margin = { top: 20, right: 20, bottom: 50, left: 100 };
+const graphWidth = 560 - margin.right - margin.left;
+const graphHeight = 360 - margin.top - margin.bottom;
+
+
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', graphWidth + margin.left + margin.right)
+  .attr('height', graphHeight + margin.top + margin.bottom);
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+// scales
+const x = d3.scaleTime().range([0, graphWidth]);
+const y = d3.scaleLinear().range([graphHeight, 0]);
+
+// axes groups
+const xAxisGroup = graph.append('g')
+  .attr('class', 'x-axis')
+  .attr('transform', "translate(0," + graphHeight + ")");
+
+const yAxisGroup = graph.append('g')
+  .attr('class', 'y-axis');
+
+// update function
+const update = (data) => {
+
+  // set scale domains
+  x.domain(d3.extent(data, d => new Date(d.date)));
+  y.domain([0, d3.max(data, d =>  d.distance)]);
+
+  //console.log(x(new Date(data[0].date)))
+
+  // create axes
+  const xAxis = d3.axisBottom(x)
+    .ticks(4)
+    .tickFormat(d3.timeFormat("%b %d"));
+    
+  const yAxis = d3.axisLeft(y)
+    .ticks(4)
+    .tickFormat(d => d + 'm');
+
+  // call axes
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+
+  // rotate axis text
+  xAxisGroup.selectAll('text')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end');
+
+};
+
+
 
 // data array and firestore
 let data = [];
@@ -93,4 +157,6 @@ const unsub = onSnapshot(colRef, (snapshot)  => {
     }
 
   })
+
+  update(data)
 });
